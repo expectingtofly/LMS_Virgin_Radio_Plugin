cd2214e80862029e6ee03a431c63ef817b1e743a
Stu <stu@expectingtofly.co.uk>
2021-02-11 19:00:08 +0000

    Working Menu

---
+++ VirginRadio/Utilities.pm
@@ -0,0 +1,9 @@
+package Plugins::VirginRadio::Utilities;
+
+use warnings;
+use strict;
+
+use constant URL_VIRGINSCHEDULE => 'https://virginradio.co.uk/radio/schedule/';
+use constant IMG_DEFAULT_IMAGE => 'https://virginradio.co.uk/sites/all/themes/virgin2019/dist/images/schedule-images-desktop/default-thumbnail.jpg';
+
+1;
