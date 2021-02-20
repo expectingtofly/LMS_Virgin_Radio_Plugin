7b572a7249203cd7fd13a939442292ce7522260e
Craig Drummond <craig.p.drummond@gmail.com>
2021-02-20 18:13:54 +0000

    Add Material-like mono SVG icon, and map others.

--- VirginRadio/Utilities.pm
+++ VirginRadio/Utilities.pm
@@ -6,6 +6,7 @@ use strict;
 use constant URL_VIRGINSCHEDULE => 'https://virginradio.co.uk/radio/schedule/';
 use constant IMG_SCHEDULE => 'plugins/VirginRadio/html/images/schedule_MTL_icon_calendar_today.png';
 use constant IMG_VIRGINRADIO => 'plugins/VirginRadio/html/images/VirginRadio.png';
+use constant IMG_VIRGINRADIO_LIVE => 'plugins/VirginRadio/html/images/live_MTL_icon_radio.png';
 use constant IMG_DEFAULT_IMAGE => 'https://virginradio.co.uk/sites/all/themes/virgin2019/dist/images/schedule-images-desktop/default-thumbnail.jpg';
 use constant IMG_VIRGINRADIOANTHEMS => 'plugins/VirginRadio/html/images/VirginRadioAnthems.png';
 use constant IMG_VIRGINRADIOCHILLED => 'plugins/VirginRadio/html/images/VirginRadioChilled.png';
