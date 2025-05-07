20e789300995800a2298d86b20765a27537791fd
Stu <stu@expectingtofly.co.uk>
2025-05-07 17:24:59 +0100

    Remove unused images

--- VirginRadio/Utilities.pm
+++ VirginRadio/Utilities.pm
@@ -3,14 +3,7 @@ package Plugins::VirginRadio::Utilities;
 use warnings;
 use strict;
 
-use constant URL_VIRGINSCHEDULE => 'https://virginradio.co.uk/schedules/virginradiouk';
 use constant IMG_SCHEDULE => 'plugins/VirginRadio/html/images/schedule_MTL_icon_calendar_today.png';
-use constant IMG_VIRGINRADIO => 'plugins/VirginRadio/html/images/VirginRadio.png';
 use constant IMG_VIRGINRADIO_LIVE => 'plugins/VirginRadio/html/images/live_MTL_icon_radio.png';
-use constant IMG_DEFAULT_IMAGE => 'https://virginradio.co.uk/sites/all/themes/virgin2019/dist/images/schedule-images-desktop/default-thumbnail.jpg';
-use constant IMG_VIRGINRADIOCHILLED => 'plugins/VirginRadio/html/images/VirginRadioChilled.png';
-use constant IMG_VIRGINRADIO80SPLUS => 'plugins/VirginRadio/html/images/VirginRadio80splus.png';
-use constant IMG_VIRGINRADIOBRITPOP => 'plugins/VirginRadio/html/images/VirginRadioBritpops.png';
-use constant IMG_VIRGINRADIOLEGENDS => 'plugins/VirginRadio/html/images/VirginRadioLegends.png';
 
 1;
